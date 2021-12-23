9d30eb0a79bae9df53ed04d5bd923246546a0460
mantielero <josemaria.alkala@gmail.com>
2021-12-23 12:41:02 +0100

    Comment unused code

--- src/lib/point3d.nim
+++ src/lib/point3d.nim
@@ -20,7 +20,7 @@ proc addPoint*[X,Y,Z:SomeNumber](sys:var System; x:X; y:Y; z:Z):Point3d =
   addPoint(sys, x, y, z, sys.currentGroup)
 
 
-proc xid*(sys:System; p:Point3d):IdParam =
+#[ proc xid*(sys:System; p:Point3d):IdParam =
   sys.getEntity(p).param[0]
 
 proc yid*(sys:System; p:Point3d):IdParam =
@@ -33,7 +33,7 @@ proc coord*(sys:System; p:Point3d):tuple[x,y,z:float] =
   let xid = sys.xid(p)
   let yid = sys.yid(p)  
   let zid = sys.zid(p)   
-  (sys.params[xid-1].val, sys.params[yid-1].val, sys.params[zid-1].val)
+  (sys.params[xid-1].val, sys.params[yid-1].val, sys.params[zid-1].val) ]#
 
 
 
