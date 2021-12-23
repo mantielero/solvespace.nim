665df3f71724381b87df1878347687edc9d9008d
mantielero <josemaria.alkala@gmail.com>
2021-12-23 12:41:30 +0100

    Adding ~= to assert floats

--- src/solvespace.nim
+++ src/solvespace.nim
@@ -5,17 +5,20 @@ const libName = "slvs"
 {.passL: "-l" &  libName.}
 
 
+import std/math
 
 import wrapper/slvs
 export slvs
 
-import lib/[constants, types, system, params, point3d, segment, cubic]
-import lib/[constraints, quaternion, drag, workplane, normal2d, normal3d, solve, point2d, arcofcircle]
-export constants, types, system, params, point3d, segment, cubic
-export constraints, quaternion, drag, workplane, normal2d, normal3d, solve, point2d, arcofcircle
+import lib/[constants, types, system, params, point2d, point3d, segment, cubic]
+import lib/[constraints, quaternion, drag, workplane, normal2d, normal3d, solve, arcofcircle]
+export constants, types, system, params, point2d, point3d, segment, cubic
+export constraints, quaternion, drag, workplane, normal2d, normal3d, solve, arcofcircle
 
 
 import lib/[distance, circle, showing]
 export distance, circle, showing
 
 
+proc `~=`*[T:SomeFloat](val1,val2:T):bool =
+  almostEqual(val1, val2)
