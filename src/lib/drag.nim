8803a5bc0bd7a39a241bb81a73ceb9e442e107fd
mantielero <josemaria.alkala@gmail.com>
2021-12-23 12:42:09 +0100

    Added point2d case

--- src/lib/drag.nim
+++ src/lib/drag.nim
@@ -1,7 +1,18 @@
 #import ../wrapper/slvs
-import types, point3d
+import types, point3d, point2d
 
-proc drag*( sys:var System; p:Point3d) =
-  sys.sys.dragged[0] = sys.xid(p)
-  sys.sys.dragged[1] = sys.yid(p)
-  sys.sys.dragged[2] = sys.zid(p)
+proc drag*( p:Point3d) =
+  p.sys.sys.dragged[0] = p.xid
+  p.sys.sys.dragged[1] = p.yid
+  p.sys.sys.dragged[2] = p.zid
+
+proc drag*( p:Point2d) =
+  p.sys.sys.dragged[0] = p.xid
+  p.sys.sys.dragged[1] = p.yid
+
+
+proc drag*( p1,p2:Point2d) =
+  p1.sys.sys.dragged[0] = p1.xid
+  p1.sys.sys.dragged[1] = p1.yid
+  p1.sys.sys.dragged[2] = p2.xid
+  p1.sys.sys.dragged[3] = p2.yid
