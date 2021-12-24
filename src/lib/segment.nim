00532280f76430e060538badabe476a35ef2a2d5
mantielero <josemaria.alkala@gmail.com>
2021-12-24 14:22:39 +0100

    removing old sys dependency

--- src/lib/segment.nim
+++ src/lib/segment.nim
@@ -12,10 +12,10 @@ proc addSegment*[T:Point3d | Point2d](sys:var System; p1,p2:T; workplane:Workpla
 proc addSegment*[T:Point3d | Point2d](sys:var System; p1,p2:T ):Segment =
   addSegment(sys, p1 , p2, sys.currentWorkplane, sys.currentGroup)
 
-proc getPoints*(sys:System; s:Segment):seq[tuple[id:IdEntity; typ:uint]] =
-  let segment = sys.getEntity(s)
+proc getPoints*(s:Segment):seq[tuple[id:IdEntity; typ:uint]] =
+  let segment = s.sys.getEntity(s)
   let pid1 = segment.point[0]
   let pid2 = segment.point[1]
-  let typ1 = sys.getEntity(segment.point[0]).`type`.uint
-  let typ2 = sys.getEntity(segment.point[1]).`type`.uint
+  let typ1 = s.sys.getEntity(segment.point[0]).`type`.uint
+  let typ2 = s.sys.getEntity(segment.point[1]).`type`.uint
   return @[(pid1, typ1), (pid2, typ2)]
