5425c72b1f33dc9b7cbbb26edcb71f021f81c9bc
mantielero <josemaria.alkala@gmail.com>
2021-12-25 11:01:29 +0100

    still not working

--- tests/constraints/test_on.nim
+++ tests/constraints/test_on.nim
@@ -10,8 +10,8 @@ proc main =
     # Circle
     let center = sys.addPoint(200, 200, 200)
     let normal = sys.addNormal( 1, 0, 0,
-                                0, 0, 1 ) 
-    let circle = sys.addCircle( center, normal, 30) 
+                                0, 1, 0 ) 
+    let circle = sys.addCircle( center, normal, 100) 
 
 
     # Lets create two horizontal line
@@ -22,9 +22,11 @@ proc main =
     let res = sys.solve
 
     assert res == rOK 
-    #echo p.coord
-    #echo center.coord
+    echo p.coord
+    echo center.coord
+    echo circle.radius
     #echo measureDistance(p, center)
+    # TODO: not working
     #assert measureDistance(p, center)  ~= 30
 
 
