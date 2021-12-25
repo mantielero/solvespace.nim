b3441eb1625bd253e2f3d192f8ed2a9a79a7c448
mantielero <josemaria.alkala@gmail.com>
2021-12-25 11:02:21 +0100

    preliminar

--- tests/constraints/test_tangent.nim
+++ tests/constraints/test_tangent.nim
@@ -3,23 +3,35 @@ import solvespace
 proc main =
   var sys = newSystem()
 
-  # Segments of equal lengths
+  # Segment tangent to Arc Of Circle
   block:
     sys.setGroup(1)
     let wp = sys.addWorkplane( 0, 0, 100, # Origin
                                1, 0, 0, # u
                                0, 1, 0) # v
+    sys.setWorkplane(wp)
 
-    let p1 = sys.addPoint(200, 200, 200)
-    let p2 = sys.addPoint(200, 200, 300)
+    # Arc of circle creation
+    let normal = wp.normalId
+    let pCenter = sys.addPoint(0, 0)
+    let pStart  = sys.addPoint(-100, 0)
+    let pFinish = sys.addPoint(100, 0)
+    let aoc = sys.addArcOfCircle(normal, pCenter, pStart, pFinish)
 
-    symmetric(p1, p2, wp)
+    # Add segment
+    let p1 = sys.addPoint(100, 200)
+    let p2 = sys.addPoint(200, 0)
+    let s = sys.addSegment(p1, p2)
 
+    tangent(s, aoc, false)
+    #echo sys.constraints
+    #sys.showEntities
     let res = sys.solve
 
     assert res == rOK
     # TODO: measure distance from point to workplane
-    
+    #echo p1
+    #echo pStart
     #echo p.coord
     #echo center.coord
     #echo measureDistance(p, center)
