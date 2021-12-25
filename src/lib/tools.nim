402af913114cf5b536ba7fcd06f75218927edffc
mantielero <josemaria.alkala@gmail.com>
2021-12-25 10:59:11 +0100

    playing with the error allowed by almostEqual

--- src/lib/tools.nim
+++ src/lib/tools.nim
@@ -3,7 +3,7 @@ import ../wrapper/slvs
 import types, point3d, point2d, segment#, workplane
 
 proc `~=`*[T:SomeFloat](val1,val2:T):bool =
-  almostEqual(val1, val2)
+  almostEqual(val1, val2) #, 5)
 
 proc measureDistance*(p1,p2:Point3d):float =
   sqrt((p1.x - p2.x)^2 + (p1.y - p2.y)^2 + (p1.z - p2.z)^2) 
