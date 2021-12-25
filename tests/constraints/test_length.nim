b9c6794dc9fe6539d2101f1193e920f0b5bc6234
mantielero <josemaria.alkala@gmail.com>
2021-12-25 11:01:03 +0100

    added more tests

--- tests/constraints/test_length.nim
+++ tests/constraints/test_length.nim
@@ -1,5 +1,5 @@
 import solvespace
-
+ 
 proc main =
   var sys = newSystem()
 
@@ -62,5 +62,75 @@ proc main =
 
     assert res == rOK 
     assert measureDistance(p1,p2) ~= (measureDistance(p3,p4) + 10.0)
+
+  # Equal distance from a point to two different segments
+  block:
+    sys.setGroup(4)
+
+    let p0 = sys.addPoint(50,50,50)
+
+    # Lets create two segments
+    let p1 = sys.addPoint(10, 10, 10)
+    let p2 = sys.addPoint(10, 10, 60)
+    let s1 = sys.addSegment(p1, p2)
+    p1.dragged
+    p2.dragged
+
+    let p3 = sys.addPoint(100, 100, 100)
+    let p4 = sys.addPoint(100, 100, 200)
+    let s2 = sys.addSegment(p3, p4) 
+    p3.dragged
+    p4.dragged
+
+
+
+    p0.equalDistance( s1, s2 )
+    echo measureDistance(p0,s1)
+    echo measureDistance(p0,s2)
+    echo "DOF: ", sys.getDOF
+    let res = sys.solve
+
+    assert res == rOK 
+    echo measureDistance(p0,s1) 
+    echo measureDistance(p0,s2)
+    echo p1.coord
+    echo p2.coord
+    echo p3.coord
+    echo p4.coord
+    echo p0.coord
+    #assert measureDistance(p0,s1) ~= measureDistance(p0,s2) 
+
+  block:
+    sys.setGroup(5)
+
+    # Lets create two segments
+    let p1 = sys.addPoint(10, 10, 10)
+    let p2 = sys.addPoint(10, 10, 60)
+    let s1 = sys.addSegment(p1, p2)
+
+    # Arc of circle
+    let normal = sys.addNormal( 1, 0, 0,
+                                0, 1, 0 ) 
+    let pCenter = sys.addPoint(100, 120, 300)
+    let pStart  = sys.addPoint(120, 110, 300)
+    let pFinish = sys.addPoint(115, 115, 300)
+    let aoc = sys.addArcOfCircle(normal, pCenter, pStart, pFinish)
+
+    equalLength( s1, aoc )
+
+    #sys.showEntities
+    let res = sys.solve
+
+    assert res == rOK 
+    #echo measureDistance(p1, p2)
+    echo "Distances"
+    echo "  - segment: ", measureDistance(p1,p2)
+    echo "     p1: ", p1.coord
+    echo "     p2: ", p2.coord    
+    echo "  - arcofcircle: ", aoc.length
+    echo "    center: ", pCenter.coord
+    echo "    start:  ", pStart.coord
+    echo "    finish: ", pFinish.coord    
+    #assert measureDistance(p1,p2) ~= aoc.length
 
 main()
